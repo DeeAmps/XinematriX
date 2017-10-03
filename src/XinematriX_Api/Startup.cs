d50eb53bb10bfd4ab91d50ce335bdce24c7e044a
Danny <danyelamps.db@gmail.com>
2017-10-03 16:49:42 -0700

    May it Be

--- src/XinematriX_Api/Startup.cs
+++ src/XinematriX_Api/Startup.cs
@@ -24,6 +24,7 @@ namespace XinematriX_Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            //services.AddDbContext
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
